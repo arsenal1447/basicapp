74c9c5cc7c3a9b0a02a25b139d8e5035d7f75048
xinxin <xinxin@rrkd.cn>
2015-08-13 16:23:48 +0800

    For basicapp:完成注册功能

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -63,7 +63,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+            <p class="pull-left">&copy; My BBS <?= date('Y') ?></p>
             <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
